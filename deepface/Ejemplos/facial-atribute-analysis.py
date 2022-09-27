b76821b85f1a900a3973592506e0ee763fe1ecf8
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-09-27 09:37:46 -0500

    Archivos agregados

--- deepface/Ejemplos/facial-atribute-analysis.py
+++
@@ -1,7 +0,0 @@
-from deepface import DeepFace
-
-obj = DeepFace.analyze (img_path = "/home/hugo/Documents/GitHub/apertura-puertas-reconocimiento-facial/deepface/ai-faces/aigeneratedface1.jpg", actions = ['age', 'gender', 'race', 'emotion'])
-
-
-print ("El resultado del analisis es: ")
-print (obj)
