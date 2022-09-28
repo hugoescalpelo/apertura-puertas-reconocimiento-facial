5dcd1f4672d98923f9c630147228a24c059c479b
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-09-28 11:07:23 -0500

    DB agregada

---
+++ deepface/Ejemplos/face-recognition.py
@@ -0,0 +1,13 @@
+# Importar Bilbioteca
+from deepface import DeepFace
+import pandas as pd
+
+# Buscar Rostro
+print ("Buscando rostro")
+
+# df = DeepFace.find(img_path = "img1.jpg", db_path = "C:/workspace/my_db")
+df = DeepFace.find (img_path = "/home/hugo/Documents/GitHub/apertura-puertas-reconocimiento-facial/deepface/faces/Rami.png", db_path = "/home/hugo/Documents/GitHub/apertura-puertas-reconocimiento-facial/deepface/my_db", enforce_detection = "false")
+print ("Resultado ")
+print (df)
+print ("Seleccion")
+print (df.identity[0])
