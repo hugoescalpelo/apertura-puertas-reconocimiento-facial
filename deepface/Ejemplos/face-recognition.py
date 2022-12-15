4d1197028e0a1154537eeded32b784fb573d4b7d
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-12-15 17:33:24 -0600

    Programas actualizados

--- deepface/Ejemplos/face-recognition.py
+++ deepface/Ejemplos/face-recognition.py
@@ -6,10 +6,10 @@ import pandas as pd
 print ("Buscando rostro")
 
 # df = DeepFace.find(img_path = "img1.jpg", db_path = "C:/workspace/my_db")
-df = DeepFace.find (img_path = "/home/hugo/Documents/GitHub/apertura-puertas-reconocimiento-facial/deepface/faces/aigeneratedface2.jpg", db_path = "/home/hugo/Documents/GitHub/apertura-puertas-reconocimiento-facial/deepface/my_db", enforce_detection = "false")
+df = DeepFace.find (img_path = "/home/hugo/Documents/GitHub/apertura-puertas-reconocimiento-facial/deepface/faces/carrie1.png", db_path = "/home/hugo/Documents/GitHub/apertura-puertas-reconocimiento-facial/deepface/my_db", enforce_detection = "false")
 print ("Resultado ")
 print (df)
 print ("Imagen de mayor similitud")
 print (df.identity[0])
-print ("Imagen de similitud secundara")
-print (df.identity[1])
+# print ("Imagen de similitud secundara")
+# print (df.identity[1])
